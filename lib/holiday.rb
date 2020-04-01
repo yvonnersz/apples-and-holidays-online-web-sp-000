64889365289154212d601ff7a513e519a3cc28a2
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 09:07:39 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,37 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1].to_s
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter][:christmas] << supply
+holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[:winter] = {:"#{holiday_name}" => supply_array}
+  holiday_hash[:summer] = {:"#{holiday_name}" => supply_array}
+  holiday_hash[:fall] = {:"#{holiday_name}" => supply_array}
+  holiday_hash[:spring] = {:"#{holiday_name}" => supply_array}
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -54,17 +60,21 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |seasons,holidays_and_supplies|
+    puts "#{seasons.to_s.capitalize}:"
+    holidays_and_supplies.each do |holidays, supplies|
+        fixed_holidays = holidays.to_s.split("_").collect {|word| word.capitalize}.join(" ")
+        puts "  #{fixed_holidays}: #{supplies.join(", ")}"
+    end
+  end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holidays_bbq = holiday_hash[:summer].keys
+  holidays_bbq << holiday_hash[:spring].keys
+  holidays_bbq.flatten
 
 end
-
-
-
-
-
-
-
