94812a93f20c2b53d090a28e37e27edf2bf6447b
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 03:01:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |seasons,holidays_and_supplies|
         fixed_holidays = holidays.to_s.split("_").collect {|word| word.capitalize}.join(" ")
+        binding.pry
         puts "#{seasons.to_s.capitalize}:"
         puts "#{fixed_holidays}: #{supplies.join(", ")}"
     end
